139cc2c5d4992c03440c1fca773f831084fd4956
BeConfused <51351592+BeConfused@users.noreply.github.com>
2026-07-31 01:18:40 +0200

    corrected type definition

--- internal/pkg/plan/plan.go
+++ internal/pkg/plan/plan.go
@@ -4,6 +4,6 @@ package plan
 import "github.com/BeConfused/base-planner/internal/pkg/dataset"
 
 // Plan exposes a set of functions to guarantee a report, based on a given plan.
-type Plan interface {
-	GetReport(c dataset.Config) Report[dataset.Entity]
+type Plan[E dataset.Entity] interface {
+	GetReport(c dataset.Config) (*Report[E], error)
 }
